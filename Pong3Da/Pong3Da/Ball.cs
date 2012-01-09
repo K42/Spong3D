be58afbda1d07589d988f6891507b145e413d328
hash <hashysh@gmail.com>
2012-01-09 20:12:52 +0100

    Ball with random direction - velocity

--- Pong3Da/Pong3Da/Ball.cs
+++ Pong3Da/Pong3Da/Ball.cs
@@ -1,39 +1,31 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 
-namespace Pong3Da
-{
+namespace Pong3Da {
     /// <summary>
     /// This is a game component that implements IUpdateable.
     /// </summary>
-    public class Ball : Microsoft.Xna.Framework.GameComponent
-    {
-        public Vector3 position;
-        public bool freeze { get; set; }
-        public float ballSpeed;
+    public class Ball : Microsoft.Xna.Framework.GameComponent {
         public Model model;
 
         protected Matrix world = Matrix.Identity;
 
-        public Vector3 velocity = Vector3.Zero;
-        public float direction = 0.0f;
+        public Vector3 ballSpeed;
+        public Vector3 position;
+        public Vector3 direction;
 
+        public bool freeze { get; set; }
 
         public Ball(Game game)
-            : base(game)
-        {
+            : base(game) {
+            Random r = new Random();
             position = Vector3.Zero;
             freeze = true;
-            ballSpeed = 0.05f;
+            ballSpeed = Vector3.One * 0.15f;
+            direction = new Vector3((float) (r.NextDouble() - 0.5), (float) (r.NextDouble() - 0.5), (float) (r.NextDouble() - 0.5));
+            direction.Normalize();
             model = Game.Content.Load<Model>(@"models\testbox");
             // TODO: Construct any child components here
         }
@@ -42,10 +34,9 @@ namespace Pong3Da
         /// Allows the game component to perform any initialization it needs to before starting
         /// to run.  This is where it can query for any required services and load content.
         /// </summary>
-        public override void Initialize()
-        {
+        public override void Initialize() {
             // TODO: Add your initialization code here
-            
+
             base.Initialize();
         }
 
@@ -53,44 +44,26 @@ namespace Pong3Da
         /// Allows the game component to update itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        public override void Update(GameTime gameTime)
-        {
+        public override void Update(GameTime gameTime) {
             //// TODO: Add your update code here
-            if (freeze != true)
-            {
+            if (freeze != true) {
                 Bounce();
             }
             base.Update(gameTime);
         }
 
 
-        private void Bounce()
-        {
-            position += velocity;
-            direction -= ballSpeed * 0.10f;
-
-            Vector3 velocityDelta = Vector3.Zero;
-
-            //wyznaczanie predkosci na podstawie kierunku
-            //TODO: trzecia wspolrzedna
-            velocityDelta.X = -(float)Math.Sin(direction);
-            velocityDelta.Z = -(float)Math.Cos(direction);
-
-            // Now scale our direction by how hard the trigger is down.
-            velocityDelta *= ballSpeed;
-            velocity += velocityDelta;
+        private void Bounce() {
+            position += ballSpeed * direction;
         }
 
         //rysowanie pilki
-        public void Draw(Camera camera)
-        {
+        public void Draw(Camera camera) {
             Matrix[] transforms = new Matrix[model.Bones.Count];
             model.CopyAbsoluteBoneTransformsTo(transforms);
 
-            foreach (ModelMesh mesh in model.Meshes)
-            {
-                foreach (BasicEffect be in mesh.Effects)
-                {
+            foreach (ModelMesh mesh in model.Meshes) {
+                foreach (BasicEffect be in mesh.Effects) {
                     be.EnableDefaultLighting();
                     be.Projection = camera.projection;
                     be.View = camera.view;
@@ -100,10 +73,9 @@ namespace Pong3Da
             }
         }
 
-        public virtual Matrix GetWorld()
-        {
+        public virtual Matrix GetWorld() {
             return world;
         }
-       
+
     }
 }
